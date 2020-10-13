23e32a8d5284a63169cf0cd30537c1fe91f93e39
Jeff Chen <jchen@ladderlife.com>
2020-10-13 07:50:26 -0600

    update website list

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,13 @@
 const whitelist = [
-  /.*reddit.com/,
-  /.*arstechnica.com/,
-  /.*youtube.com/,
+  /reddit\.com/,
+  /arstechnica\.com/,
+  /youtube\.com/,
+  /twitter\.com/,
+  /facebook\.com/,
+  /theringer\.com/,
+  /news\.ycombinator\.com/,
+  /hckrnews\.com/,
+  /anandtech\.com/,
 ];
 
 function shouldShowFrame(domain) {
@@ -15,7 +21,7 @@ function onmessage(msg) {
     const frame = document.createElement("iframe");
     frame.src = chrome.runtime.getURL("src/frame.html");
     frame.style =
-      "position: fixed; bottom: 10px; right: 10px; border: 1px solid grey; border-radius: 5px; z-index: 99999; height: 50px; width: 150px;";
+      "position: fixed; bottom: 10px; right: 10px; border: 1px solid grey; border-radius: 5px; z-index: 99999; height: 30px; width: 150px;";
     frame.name = domain;
     document.body.insertBefore(frame, document.body.firstChild);
   }
