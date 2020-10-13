5b9ed1169ca436614a52dad4fe11e4cf0e91969a
Jeff Chen <jchen@ladderlife.com>
2020-10-13 12:42:43 -0600

    only add time when the chrome window is focused

--- src/bg/background.js
+++ src/bg/background.js
@@ -2,8 +2,12 @@ function interval() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     tabs.forEach((tab) => {
       const domain = new URL(tab.url).hostname;
-      chrome.storage.local.get(domain, (res) => {
-        chrome.storage.local.set({ [domain]: (res[domain] || 0) + 1 });
+      chrome.windows.get(tab.windowId, (window) => {
+        if (window.focused) {
+          chrome.storage.local.get(domain, (res) => {
+            chrome.storage.local.set({ [domain]: (res[domain] || 0) + 1 });
+          });
+        }
       });
     });
   });
