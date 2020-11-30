de39c56435381780daf38a91760373494cbbe978
Jeff Chen <jchen@ladderlife.com>
2020-11-30 11:10:03 -0800

    3x

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,7 +5,7 @@ function onmessage(msg) {
     const frame = document.createElement("iframe");
     frame.src = chrome.runtime.getURL("src/frame.html");
     frame.style =
-      "position: fixed; bottom: 10px; right: 10px; border: 1px solid grey; border-radius: 5px; z-index: 2147483647; height: 50px; width: 150px;";
+      "position: fixed; bottom: 10px; right: 10px; border: 1px solid grey; border-radius: 15px; z-index: 2147483647; height: 150px; width: 430px;";
     frame.name = domain;
     document.body.insertBefore(frame, document.body.firstChild);
   }
